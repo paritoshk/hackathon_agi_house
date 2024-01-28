e048cd57e6a84e363d2757a2073e09e019ce0dac
Paritosh Kulkarni <pck2123@columbia.edu>
2024-01-27 22:09:10 -0800

    Update embed.py

--- app/db/embed.py
+++ app/db/embed.py
@@ -11,7 +11,7 @@ def generate_embedding(query: str):
     headers = {
         "accept": "application/json",
         "content-type": "application/json",
-        "Authorization": "Bearer 02f64462e7d099ceadef8761a30afe2779db1027eddfec587869f4ea28a97013"
+        "Authorization": f"Bearer BEARER"
     }
 
 
