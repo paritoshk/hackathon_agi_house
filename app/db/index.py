3d5c590d237d0f84286d57292340f523c15a693f
paritoshk <pck2123@columbia.edu>
2024-01-27 19:00:45 -0800

    removed embed

--- app/db/index.py
+++ app/db/index.py
@@ -2,8 +2,30 @@ import argparse
 import pymongo
 import os
 import time
+import requests
 
-from embed import generate_embedding
+def generate_embedding(query: str):
+    url = "https://api.together.xyz/v1/embeddings"
+
+    payload = {
+        "model": "togethercomputer/m2-bert-80M-8k-retrieval",
+        "input": query
+    }
+
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "Authorization": "Bearer 02f64462e7d099ceadef8761a30afe2779db1027eddfec587869f4ea28a97013"
+    }
+
+
+    r = requests.post(url, json=payload, headers=headers)
+    if r.status_code != 200:
+        print(f'Failed to get embedding for {query}')
+        print(r.json())
+    # print(r.status_code)
+    return r.json()['data'][0]['embedding']
+
 
 # parser = argparse.ArgumentParser(description='Compute embeddings for a directory')
 # parser.add_argument('--path', type=str, help='path of directory to embed')
