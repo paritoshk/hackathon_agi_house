b7dea1444f2218454485a0440efd9b55be7168d2
Praneet Rathi <prrathi10@gmail.com>
2024-01-27 17:37:50 -0800

    moved leon files

--- db/mongo.py
+++ db/mongo.py
@@ -25,33 +25,11 @@ def generate_embedding(query: str):
 def generate_instruction(query: str):
     pass
 
-# print(generate_embedding("cat"))
-
 for doc in embeddings.find():
     print(doc['name'])
 
-# embeddings.insert_one({"name": "cat", "embedding": generate_embedding("cat")})
-
-# from openai import OpenAI
-
-# TOGETHER_API_KEY = '02f64462e7d099ceadef8761a30afe2779db1027eddfec587869f4ea28a97013'
-
-# client = OpenAI(
-#     api_key=TOGETHER_API_KEY,
-#     base_url = 'https://api.together.xyz/'
-# )
-
-# client.
-
-results = embeddings.aggregate([
+results = list(embeddings.aggregate([
     {
-        # '$vectorSearch': {
-        #     'queryVector': generate_embedding("import math"),
-        #     'path': 'File Embeddings',
-        #     'numCandidates': 5,
-        #     'limit': 5,
-        #     'index': 'vector_index'
-        # }
         "$vectorSearch": {
             "index": "vector_index",
             "path": "File Embeddings",
@@ -60,9 +38,11 @@ results = embeddings.aggregate([
             "limit": 5
         }
     }
-])
+]))
 
-print(list(results))
-# for document in results:
-#     print(document)
+print("here")
+print(results)
+
+for document in results:
+    print(document)
 
